7c0edd7c4f585503c667e83b2fb74fb8fbcd5773
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-04-09 17:43:03 +0900

    Capistrano set up

---
+++ config/deploy.rb
@@ -0,0 +1,31 @@
+lock '3.12.1'
+
+set :application, 'dear_wack_hp'
+set :repo_url,  'git@github.com:mother-art/dear_wack_hp.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/ChatSpace.pem'] 
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
+end
