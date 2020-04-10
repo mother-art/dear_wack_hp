9f04eb7f21dc1911034a217be87deea8f08fb758
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-04-10 13:01:08 +0900

    deploy.rb fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,23 @@
 lock '3.12.1'
 
 set :application, 'dear_wack_hp'
+
 set :repo_url,  'git@github.com:mother-art/dear_wack_hp.git'
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
-set :ssh_options, auth_methods: ['publickey'], keys: ['~/.ssh/ChatSpace.pem'] 
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/ChatSpace.pem'] 
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
+
+set :linked_files, %w{ config/master.key }
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
@@ -17,13 +26,13 @@ namespace :deploy do
     invoke 'unicorn:start'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+      upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
