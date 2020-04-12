bebcd29922fa346d6eddcc350f881654821ab96b
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-04-12 13:44:52 +0900

    profile-page-create

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,35 +1,25 @@
-// $(document).on('turbolinks:load', ()=> {
-  $(function() {
-    $(".menu__know").hover(function() {
-      console.log("s")
-      $(".menu__know__list:not(:animated)", this).slideDown();
-      console.log($(this).children(".menu__know__list"))
-    }, function() {
-      $(".menu__know__list", this).slideUp();
-      });
-    
-    $(".menu__sns").hover(function() {
-      console.log("s")
-      $(".menu__sns__list:not(:animated)", this).slideDown();
-      console.log($(this).children(".menu__sns__list"))
-    }, function() {
-      $(".menu__sns__list", this).slideUp();
-      });
-    
-    $(".menu__live").hover(function() {
-      console.log("s")
-      $(".menu__live__list:not(:animated)", this).slideDown();
-      console.log($(this).children(".menu__live__list"))
-    }, function() {
-      $(".menu__live__list", this).slideUp();
-      });
-
-    $(".menu__youtube").hover(function() {
-      console.log("s")
-      $(".menu__youtube__list:not(:animated)", this).slideDown();
-      console.log($(this).children(".menu__youtube__list"))
-    }, function() {
-      $(".menu__youtube__list", this).slideUp();
+$(function() {
+  $(".menu__know").hover(function() {
+    $(".menu__know__list:not(:animated)", this).slideDown();
+  }, function() {
+    $(".menu__know__list", this).slideUp();
     });
+  
+  $(".menu__sns").hover(function() {
+    $(".menu__sns__list:not(:animated)", this).slideDown();
+  }, function() {
+    $(".menu__sns__list", this).slideUp();
+    });
+  
+  $(".menu__live").hover(function() {
+    $(".menu__live__list:not(:animated)", this).slideDown();
+  }, function() {
+    $(".menu__live__list", this).slideUp();
+    });
+
+  $(".menu__youtube").hover(function() {
+    $(".menu__youtube__list:not(:animated)", this).slideDown();
+  }, function() {
+    $(".menu__youtube__list", this).slideUp();
   });
-// });
+});
