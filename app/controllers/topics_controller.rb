7e487220eeff690e7a3dfc9ffd79b471e2adc4f8
mother-art <doppel-w-8oso@ezweb.ne.jp>
2020-04-16 02:30:40 +0900

    topics_new-create

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,16 +1,31 @@
 class TopicsController < ApplicationController
 
   def index
+    @topics = Topic.all
   end
 
   def new
     @topic = Topic.new
   end
 
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to root_path
+    else
+      redirect_to "/topics/new"
+    end
+  end
+
   def show
   end
 
   def profile
+  end
+
+  private
+  def topic_params
+    params.require(:topic).permit(:title, :content).merge(user_id: current_user.id)
   end
 
 end
